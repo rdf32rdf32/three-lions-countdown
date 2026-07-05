767b1fe0caed90a5c48d06ca107e16153debef84
rdf32rdf32 <rdf32rdf32@gmail.com>
2026-07-05 20:57:01 +0200

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,1 +1,18 @@
-Upload these files to the root of your GitHub england-countdown repository, replacing the old files.
+England Match Centre v4
+
+Upload these files to the root of:
+https://github.com/rdf32rdf32/england-countdown
+
+Replace the old files and commit changes.
+
+Public link:
+https://rdf32rdf32.github.io/england-countdown/
+
+Version 4 adds:
+- polished England Match Centre layout
+- automatic next known England slot
+- WhatsApp share button
+- copy link button
+- iPhone/Android home-screen instructions
+- multiple alarm sounds
+- Designed by R. Freedman and copyright footer
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,13 @@
-self.addEventListener('fetch', event => {});
+const CACHE_NAME = "england-match-centre-v4";
+const FILES = ["./", "./index.html", "./manifest.json"];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
